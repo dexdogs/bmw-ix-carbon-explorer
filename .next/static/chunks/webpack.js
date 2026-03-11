2c0049a277f0da8ed5449e2b508ab657e3568e20
dexdogs <240069680+dexdogs@users.noreply.github.com>
2026-03-11 19:29:20 +0000

    fix: wheels, brakes, battery dot positions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "697788114737efb0"; }
+/******/ 		__webpack_require__.h = function() { return "0c4f5a8ac8c19ceb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
