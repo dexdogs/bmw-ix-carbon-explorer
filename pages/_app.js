13b9f883b1be8e1d882e57df67557cc28d120c3c
dexdogs <240069680+dexdogs@users.noreply.github.com>
2026-03-11 21:23:44 +0000

    mobile: force desktop viewport width=1440

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=1440" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
