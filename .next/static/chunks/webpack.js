185b6b2281e1214ca171318e3aae83130066210f
dexdogs <240069680+dexdogs@users.noreply.github.com>
2026-03-11 19:31:50 +0000

    fix: brakes up 10, battery up 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5ebbca3d50cd4e9b"; }
+/******/ 		__webpack_require__.h = function() { return "ae907e5da8f0dde1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
